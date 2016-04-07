afaea3aa5f1132ab1cdfcc9335ee4f46ad7c8478
Michael Schouten <mikeanschouten@gmail.com>
2016-04-08 00:07:18 +0200

    setup iOS

---
+++ VIiegerAppiOS/VIiegerAppiOS/settings.swift
@@ -0,0 +1,24 @@
+//
+//  settings.swift
+//  VIiegerAppiOS
+//
+//  Created by Michael Schouten on 07/04/16.
+//  Copyright © 2016 Michael Schouten. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let colorPrimary = UIColor(red:0.25, green:0.71, blue:0.25, alpha:1.0)
+let colorPrimaryDark = UIColor(red:0.2, green:0.62, blue:0.19, alpha:1.0)
+let colorAccent = UIColor(red:0.25, green:1.0, blue:0.26, alpha:1.0)
+
+let black_semi_transparent = UIColor(white: 0.0, alpha: 0.5)
+let half_black = UIColor(white:0.5, alpha:1.0)
+let white = UIColor(white:0.98, alpha:1.0)
+let white_pressed = UIColor(white:0.95, alpha:1.0)
+let red = UIColor(red:1.0, green:0.25, blue:0.51, alpha:1.0)
+let red_pressed = UIColor(red:1.0, green:0.53, blue:0.69, alpha:1.0)
+
+let SCREENHEIGHT = UIScreen.mainScreen().bounds.size.height
+let SCREENWIDTH = UIScreen.mainScreen().bounds.size.width
