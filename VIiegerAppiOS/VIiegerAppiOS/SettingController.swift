64b37afffa46657b52c53d169fba881b2c5fce6c
Michael Schouten <mikeanschouten@gmail.com>
2016-04-11 17:50:41 +0200

    bla

---
+++ VIiegerAppiOS/VIiegerAppiOS/SettingController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingController.swift
+//  VIiegerAppiOS
+//
+//  Created by Michael Schouten on 11/04/16.
+//  Copyright © 2016 Michael Schouten. All rights reserved.
+//
+import UIKit
+
+class SettingController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+}
