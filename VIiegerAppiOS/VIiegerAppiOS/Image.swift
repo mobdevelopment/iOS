0cb3d2780cfcaa2b361ad8c0ea7c2b33c528ec13
Michael Schouten <mikeanschouten@gmail.com>
2016-06-07 00:25:24 +0200

    models toegevoegd

---
+++ VIiegerAppiOS/VIiegerAppiOS/Image.swift
@@ -0,0 +1,17 @@
+//
+//  Image.swift
+//  VIiegerAppiOS
+//
+//  Created by Michael Schouten on 06/06/16.
+//  Copyright © 2016 Michael Schouten. All rights reserved.
+//
+
+import Foundation
+
+class Image: NSObject {
+    
+    var _id: Int?
+    var path: String?
+    var correct: Bool?
+    
+}
---
+++ VIiegerAppiOS/VIiegerAppiOS/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  VIiegerAppiOS
+//
+//  Created by Michael Schouten on 06/06/16.
+//  Copyright © 2016 Michael Schouten. All rights reserved.
+//
+
+import Foundation
+
+class Question: NSObject {
+    
+    var _id: Int?
+    var altitude: Image?
+    var compass: Image?
+    var images: [Image]?
+    
+}
